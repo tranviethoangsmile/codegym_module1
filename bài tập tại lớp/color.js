dcd8fa68df0bbe002429807fdb206a5131af3187
tranviethoangsmile <81605215+tranviethoangsmile@users.noreply.github.com>
2021-06-29 14:38:48 +0700

    update

--- bài tập tại lớp/color.js
+++ bài tập tại lớp/color.js
@@ -3,4 +3,7 @@ function colorNew() {
     var green = document.getElementById("green").value;
     var blue = document.getElementById("blue").value;
     document.getElementById("colorTest").style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+    document.getElementById("r").innerHTML = red;
+    document.getElementById("g").innerHTML = green;
+    document.getElementById("b").innerHTML = blue;
 }
